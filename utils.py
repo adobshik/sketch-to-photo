ee0357d0264c567bf10bccd673ef767bdf246ae6
adobshik <annadobshik@gmail.com>
2021-08-30 11:27:15 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def loss_writer(path, train_loss, val_loss):
+    with open(path, "a") as file:
+        file.write('Train loss = {}, Val loss = {} \n'.format(str(np.round(train_loss, 3)),
+                                                              str(np.round(val_loss, 3))))
+
+       
