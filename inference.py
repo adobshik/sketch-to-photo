fff3336d139a098751d0ecffad91e4c62bf39f8a
adobshik <annadobshik@gmail.com>
2021-08-31 11:37:41 +0700

    Update inference.py

--- inference.py
+++ inference.py
@@ -5,29 +5,30 @@ from skimage import io, transform
 from model_generator import Generator
 from torchvision.utils import save_image
 
+
 class Inference:
     def __init__(self, args_inference, args):
         self.args_inference = args_inference
         self.device = args['device'] if torch.cuda.is_available() else 'cpu'
         self.MODEL_PATH = args_inference.MODEL_PATH
-        self.gen = Generator(in_channels=3, features=64).to(self.device)  
+        self.gen = Generator(in_channels=3, features=64).to(self.device)
         self.load_gen()
-       
+
     def load_gen(self):
         print("=> Loading checkpoint")
         checkpoint_gen = torch.load(self.args_inference.MODEL_PATH, map_location='cpu')
         self.gen.load_state_dict(checkpoint_gen["state_dict"])
         print('Checkpoint was restored!')
-        
+
     def prepare(self, path_to_image):
-        image = io.imread(path_to_image) 
+        image = io.imread(path_to_image)
         image = transform.resize(image=image, output_shape=(256,256), order=1)
         image = torch.FloatTensor(image)
         if len(image.shape) == 2:
             image = torch.stack([image] * 3)
         else:
             image = image.permute(2, 0, 1)
-       
+
         image = image[None, :]
         return image
 
@@ -45,10 +46,10 @@ if __name__ == "__main__":
     parser.add_argument('--MODEL_PATH', type=str, help='Путь до предобученного генератора')
     parser.add_argument('--IMAGE_PATH', type=str, help='Путь до картинки (эскиза).')
     parser.add_argument('--SAVE_PATH', type=str, help='Путь для сохранения синтезированной фотореалистической версии.')
-    
+
     args_inference = parser.parse_args()
     with open(r'config.yaml') as file:
         args = yaml.load(file)
-            
+
     inference = Inference(args_inference, args)
     inference.inference(path_to_image=args_inference.IMAGE_PATH, save_path=args_inference.SAVE_PATH)
