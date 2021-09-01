c7731dcdf235a09a1eeeea46bcb8bf185be15a2f
adobshik <annadobshik@gmail.com>
2021-09-01 14:19:38 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ import numpy as np
 
 
 def loss_writer(path, train_loss, val_loss):
+    ''' Сохраняет в текстовый файл значения функций потерь 
+    Args:
+    path (str): путь до текстового файла
+    train_loss (float): значение функции потерь на тренировочных данных
+    val_loss (float): значение функции потерь на валидационных данных
+    '''
     with open(path, "a") as file:
         file.write('Train loss = {}, Val loss = {} \n'.format(str(np.round(train_loss, 3)),
                                                               str(np.round(val_loss, 3))))
